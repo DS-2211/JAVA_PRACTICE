d9bf31eae525ecb10c543d4e5149a9bdf91b3f3b
Dwarakeshwar S <dwarakeshwar4444@gmail.com>
2025-06-19 22:23:24 +0530

    init commit

---
+++ src/SingleNumber.java
@@ -0,0 +1,16 @@
+public class SingleNumber {
+        public static void main(String[] args) {
+            int[] arr = {1,4,7,6,6,4,1};
+            System.out.println( findUnique(arr) );
+
+        }
+
+        static int findUnique(int[] arr){
+            int res =0;
+            for(int i=0;i<arr.length;i++){
+                res^=arr[i];
+            }
+            return res;
+        }
+
+}
