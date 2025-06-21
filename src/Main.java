1d115f5ba13b4d4d07385442a550c148a37f9a47
Dwarakeshwar S <dwarakeshwar4444@gmail.com>
2025-06-21 23:35:21 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,43 @@
-class Main{
-    static void fun(int n){
-        if(n==0) return;
-        System.out.println(n%2);
-        fun(n/2);
-    }
+class Main {
+
     public static void main(String[] args) {
-        fun(25);
+        int[] arr = {222, 2, 1};
+        System.out.println(pivot(arr));
+    }
+
+    static int pivot(int[] arr) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+
+            if (arr.length == 1) return arr[0];
+
+
+            if (mid == 0) {
+                if (arr[0] > arr[1]) return arr[0];
+                else low = mid + 1;
+            }
+
+
+            else if (mid == arr.length - 1) {
+                if (arr[mid] > arr[mid - 1]) return arr[mid];
+                else high = mid - 1;
+            }
+
+
+            else {
+                if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                    return arr[mid];
+                } else if (arr[mid] < arr[mid + 1]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
+
+        return -1;
+    }
 }
