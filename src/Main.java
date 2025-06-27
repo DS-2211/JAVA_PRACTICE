11dd1e85a83a8761e2fc302e157b4199576dbefe
DWRK <dwarakeshwar4444@gmail.com>
2025-06-27 22:58:04 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -11,20 +11,14 @@ class Main {
             int mid = low + (high - low) / 2;
 
             if (arr.length == 1) return arr[0];
-
-
             if (mid == 0) {
                 if (arr[0] > arr[1]) return arr[0];
                 else low = mid + 1;
             }
-
-
             else if (mid == arr.length - 1) {
                 if (arr[mid] > arr[mid - 1]) return arr[mid];
                 else high = mid - 1;
             }
-
-
             else {
                 if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
                     return arr[mid];
@@ -35,7 +29,6 @@ class Main {
                 }
             }
         }
-
         return -1;
     }
 }
